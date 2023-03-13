854749b97acbdcd03ea8dcd6b3a380477407e888
Your Name <you@example.com>
2023-03-13 11:03:38 +0100

    react-countup removed

--- src/pages/PageSkills.tsx
+++ src/pages/PageSkills.tsx
@@ -2,7 +2,6 @@ import "../styles/pages/pageSkills.scss";
 import { useContext } from "react";
 import { AppContext, AppProvider } from "../AppContext";
 import ReactSkillbar from "react-skillbars";
-import CountUp from "react-countup";
 import ParallaxLine from "../components/ParallaxLine";
 
 export const PageSkills = () => {
@@ -22,17 +21,7 @@ export const PageSkills = () => {
           animationDuration={1800}
         />
       </div>
-      {/* <div className="skill">
-        {skills.map((skill, i) => {
-          return (
-            <div className="countUp" key={i}>
-              <h1 className="skillPercent">
-                <CountUp start={0} end={skill.value} duration={3} delay={0} />
-              </h1>
-            </div>
-          );
-        })}
-      </div> */}
+
       <div style={{ paddingBottom: "2rem" }}>
         <AppProvider speed={10} start={1800} end={2900}>
           <ParallaxLine />
