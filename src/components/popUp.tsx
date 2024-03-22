d4a130fc971e3b592e43d510902491442f314b4a
AbdulSamman <sammanab@outlook.de>
2024-03-22 05:37:06 +0100

    popup works

--- src/components/popUp.tsx
+++ src/components/popUp.tsx
@@ -2,60 +2,51 @@ import Swal from "sweetalert2";
 import "../styles/pages/popUp.scss";
 import { IProject } from "../interfaces";
 import { AiFillGithub, AiOutlineGlobal } from "react-icons/ai";
-import ReactDOMServer from "react-dom/server";
+import ReactDOM from "react-dom";
 
 export const popUp = (project: IProject) => {
-  // jsx to html
-  const githubIcon = ReactDOMServer.renderToString(<AiFillGithub />);
-  const globalIcon = ReactDOMServer.renderToString(<AiOutlineGlobal />);
-  Swal.fire({
-    html: `
- <div class="popupContainer">
+  const popupContainer = document.createElement("div");
 
-        <div class="popUpImage">
-            <img src=${project.image} />
-        </div>
+  ReactDOM.render(
+    <div className="popupContainer">
+      <div className="popUpImage">
+        <img src={project.image} />
+      </div>
 
-
-        <div class="tags">
-                    ${project.tags.map((tag, i) => {
-                      return (
-                        <div key={i + 1} className={`tag ${tag}`}>
-                          {tag}
-                        </div>
-                      );
-                    })}
-         </div>
-
-        <div class="row">
-                <p class="description">${project.description}</p>
-                <div class="icons">
-                  <a href=${project.repo} target="_blank">
-                   ${githubIcon}
-                  </a>
-
-                  ${
-                    project.url === "" ? (
-                      <span
-                        className={
-                          project.url === "" ? "icon notUrl" : "icon ic2"
-                        }>
-                        {globalIcon}
-                      </span>
-                    ) : (
-                      <a href={project.url} target="_blank">
-                        <span className="icon ic2">{globalIcon}</span>
-                      </a>
-                    )
-                  }
-                   </div>
-
+      <div className="tags">
+        {project.tags.map((tag, i) => {
+          return (
+            <div key={i + 1} className={`tag ${tag}`}>
+              {tag}
             </div>
-    </div>
+          );
+        })}
+      </div>
 
-
-      `,
-
+      <div className="row">
+        <p className="description">${project.description}</p>
+        <div className="icons">
+          <a href={project.repo} target="_blank">
+            <AiFillGithub className="icon ic1" />
+          </a>
+          {project.url === "" ? (
+            <AiOutlineGlobal
+              className={project.url === "" ? "icon notUrl" : "icon ic2"}
+            />
+          ) : (
+            <a href={project.url} target="_blank">
+              <AiOutlineGlobal
+                className={project.url === "" ? "icon notUrl" : "icon ic2"}
+              />
+            </a>
+          )}
+        </div>
+      </div>
+    </div>,
+    popupContainer
+  );
+  Swal.fire({
+    html: popupContainer,
     background: "rgba(0,0,50,0.9)",
     confirmButtonText: "close",
     padding: "0",
