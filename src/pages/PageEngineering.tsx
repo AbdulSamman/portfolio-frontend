04084bcb95af7a79bdb17f4317faac96b9c7880e
AbdulSamman <sammanab@outlook.de>
2026-08-29 02:25:01 +0200

    test

--- src/pages/PageEngineering.tsx
+++ src/pages/PageEngineering.tsx
@@ -1,18 +1,27 @@
 import "../styles/pages/pageProjects.scss";
 import { AppProvider } from "../AppContext";
-import React from "react";
+import React, { useState } from "react";
 import ParallaxLine from "../components/ParallaxLine";
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
 import { popUpEngineering } from "../components/Popupengineering";
 
+const FEATURED_COUNT = 6;
+
 export const PageEngineering = () => {
   const { engineeringProjects } = useContext(AppContext);
+  const [showAll, setShowAll] = useState(false);
+
+  const hasMore = engineeringProjects.length > FEATURED_COUNT;
+  const visibleProjects = showAll
+    ? engineeringProjects
+    : engineeringProjects.slice(0, FEATURED_COUNT);
+
   return (
     <div id="engineering" className="pageProjects">
       <h1>ENGINEERING</h1>
       <div className="projects">
-        {engineeringProjects.map((project) => {
+        {visibleProjects.map((project) => {
           return (
             <React.Fragment key={project._id}>
               <div
@@ -25,6 +34,16 @@ export const PageEngineering = () => {
           );
         })}
       </div>
+
+      {hasMore && (
+        <button
+          type="button"
+          className="viewAllBtn"
+          onClick={() => setShowAll((prev) => !prev)}
+        >
+          {showAll ? "Hide All" : `View All (${engineeringProjects.length})`}
+        </button>
+      )}
 
       <div className="line">
         <AppProvider speed={10} start={1900} end={3100}>
