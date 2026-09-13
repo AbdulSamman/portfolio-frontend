5e326a0653b8ae85601cae81f29cbf5897c1dcf5
AbdulSamman <sammanab@outlook.de>
2026-09-13 05:14:12 +0200

    test

--- src/pages/PageFooter.tsx
+++ src/pages/PageFooter.tsx
@@ -4,7 +4,7 @@ export const PageFooter = () => {
   return (
     <div className="PageFooter">
       <p>
-        <span> Abdulrazak Samman</span> © 2026 Alle Rechte vorbehalten.
+        <span> Abdulrazak Samman</span> © 2026 All rights reserved.
       </p>
     </div>
   );
